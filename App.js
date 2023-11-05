ed76c9ba32397e766327a698b014728c54bfc6a4
jayeshpawar04 <54260977+jayeshpawar04@users.noreply.github.com>
2023-11-05 12:19:21 +0530

    integrated parcel

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+
+const heading = React.createElement("h1", null, "HI jp 3");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
